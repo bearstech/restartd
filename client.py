136193ea43ba7e99f34a6859488bd77cd8b94e84
Mathieu Lecarme <mlecarme@bearstech.com>
2016-05-25 00:02:27 +0200

    Options

--- client.py
+++ client.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import socket
+import sys
+
+"""
+First is socker name, trailing is the message
+
+./client.py pim Hello World
+
+"""
 
 
 client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-client.connect("/tmp/pim")
-client.send(b"Hello world")
+client.connect("/tmp/%s" % sys.argv[1])
+client.send(bytes(" ".join(sys.argv[2:]), 'utf8'))
 r = client.recv(1024)
 print(r)
 client.close()
